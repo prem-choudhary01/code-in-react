d48cac0ff788d0c699d40b440b6ad471022b4570
prem Choudhary <pk5480524@gmail.com>
2024-09-11 11:41:29 +0530

    axios api

---
+++ axiosReact/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+// import { useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [country, setCountry] = useState([]);
+  const [isError, setIsError] = useState("");
+
+  // using Async Await
+  
+  useEffect(() => {
+    const getApiData = async () => {
+      try {
+        const res = await axios.get("https://api.first.org/data/v1/countries");
+        setCountry(res.data);
+        // console.log(setCountry);
+      } catch (error) {
+        setIsError(error.message);
+      }
+    };
+
+    getApiData();
+  }, []);
+  return (
+    <>
+     
+    </>
+  );
+}
+
+export default App;
