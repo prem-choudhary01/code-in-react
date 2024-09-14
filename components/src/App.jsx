dc6cbc3a305315bda8bff94c7a80713c321fdf65
prem Choudhary <pk5480524@gmail.com>
2024-09-14 20:08:23 +0530

    new change

--- components/src/App.jsx
+++ components/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import "./App.css";
 import ComponentsReUse from "./components/components";
 import Profile from "./components/profile";
+import ComponentsUse from "./components/componentsUse";
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -11,7 +12,11 @@ function App() {
     <>
      {/* <ComponentsReUse/> */}
      <Profile/>
+     <Profile/>
+     {/* <Profile/>
+     <Profile/> */}
      {/* <ComponentsReUse/> */}
+     <ComponentsUse/>
     </>
   );
 }
---
+++ components/src/components/componentsUse.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function ComponentsUse() {
+  return (
+    <>
+        <div id="modal">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+      {/* <center>
+        <div className="w-1/2 h-64 bg-gray-200 flex flex-wrap">
+          <div className="w-1/2 h-1/2 bg-red-500">Hello div one</div>
+          <div className="w-1/2 h-1/2 bg-red-500">Hello div two</div>
+          <div className="w-1/2 h-1/2 bg-red-500">Hello div three</div>
+          <div className="w-1/2 h-1/2 bg-red-500">Hello div four</div>
+        </div>
+      </center> */}
+    </>
+  );
+}
+
+export default ComponentsUse;
