b78ebf237760839011c65ace06ef60441a8093a8
prem Choudhary <pk5480524@gmail.com>
2024-09-17 12:45:59 +0530

    add array list

--- exercise/src/App.jsx
+++ exercise/src/App.jsx
@@ -1,15 +1,9 @@
-import { useState } from "react";
 import "./App.css";
 import Exercise from "./exercise";
 
-
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    // <Appuse/>
-    <Exercise/>
-  );
+  return <Exercise/>
+  
 }
 
 export default App;
--- exercise/src/one.jsx
+++ exercise/src/one.jsx
@@ -1,43 +1,20 @@
-// import React from 'react';
-
-// const MyComponent = () => {
-//   const items = [
-//     { id: 1, name: 'Item 1' },
-//     { id: 2, name: 'Item 2' },
-//     { id: 3, name: 'Item 3' },
-//     { id: 4, name: 'Item 4' }
-//   ];
-
-//   return (
-//     <div>
-//       {items.map(item => (
-//         <div key={item.id}>{item.name}</div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default MyComponent;
-
-
-// App.js
-import React from 'react';
-import ReusableDiv from './ReusableDiv';
-
-const Appuse = () => {
+const Appuse = () => { 
   const items = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
 
   return (
     <div>
+        <li>
       {items.map((items, index) => (
-        <ReusableDiv 
+      
           key={index} 
         >
           {items}
-        </ReusableDiv>
+       
       ))}
+      </li>
     </div>
   );
 };
 
 export default Appuse;
+
