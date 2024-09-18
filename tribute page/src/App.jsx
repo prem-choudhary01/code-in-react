01164b6c77e7372f67ed81f4c4e257fe2c0a99c1
prem Choudhary <pk5480524@gmail.com>
2024-09-18 18:43:42 +0530

    tribute page

---
+++ tribute page/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import OnePageProfile from './components/profile'
+// import TributePage from './components/TributePage'
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      {/* <h1>Tribute Page</h1> */}
+      {/* <TributePage/> */}
+      <OnePageProfile/>
+    </>
+  )
+}
+
+export default App
---
+++ tribute page/src/components/profile.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+const OnePageProfile = () => {
+  return (
+    <>
+      <div id="parent">
+        <header>
+          <h1>Narendra modi</h1>
+          <p>prime minister of india</p>
+        </header>
+
+        <section id="boigraphy">
+          <img src="https://static.theprint.in/wp-content/uploads/2021/08/Modi-I-Day.jpg" />
+          <p>
+            Narendra Modi, born on September 17, 1950, in Vadnagar, Gujarat, is
+            the current Prime Minister of India. He has been in office since May
+            26, 201412. Before becoming Prime Minister, he served as the Chief
+            Minister of Gujarat from 2001 to 201412. Modi’s early life was
+            marked by humble beginnings. He helped his father sell tea at a
+            railway station and later ran a tea stall with his brother2. He
+            joined the Rashtriya Swayamsevak Sangh (RSS) at a young age, which
+            significantly influenced his political career2. Modi holds a
+            Bachelor’s degree from Delhi University and a Master’s degree in
+            Political Science from Gujarat University2. His tenure as Chief
+            Minister of Gujarat was noted for economic growth and development,
+            but it was also marred by controversy, particularly the 2002 Gujarat
+            riots2. As Prime Minister, Modi has launched several key
+            initiatives, including the Swachh Bharat Abhiyan (Clean India
+            Mission), Make in India, and Digital India2
+          </p>
+        </section>
+
+        <section id="modievent">
+          <h1>
+            Upcoming event</h1>
+          <ul>
+            <li>
+              PM Modi on US visit from Sept 21-23, to attend Quad Summit,
+              address UN event Modi will attend the fourth Quad leaders' Summit
+              at Wilmington in Delaware on September 21 that is being hosted by
+              US President Joe Biden, the Ministry of External Affairs (MEA)
+              said announcing the prime minister's trip.
+            </li>
+            <br/>
+            <li>
+            A person familiar with the development said that around 15,000 people are expected to attend the gathering that will be “unique and different” in its format, flow and themes compared to past events that Modi has addressed in the US. “The main aim of the event is to celebrate the diaspora, their progress and future and ties with India.” Aware that the event is being planned in the middle of the American election season, in a departure from the past, organisers intend to keep it avowedly non-political. “There will be no elected officials invited for the event this time.”
+            </li>
+          </ul>
+        </section>
+        <footer>
+        <p>
+            
+          <button>
+                <a href="https://en.wikipedia.org/wiki/Narendra_Modi">
+                    go to wikipedia
+                </a>
+          </button>
+           
+          </p>
+        </footer>
+      </div>
+    </>
+  );
+};
+export default OnePageProfile;
