47cec958600feacdcd717730571fa4f7c89a37c5
prem Choudhary <pk5480524@gmail.com>
2024-09-20 16:58:28 +0530

    nav+Clock

---
+++ projectTest/src/components/Digital.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+function Clock() {
+  let time = new Date().toLocaleTimeString();
+
+  const [ctime, setTime] = useState(time);
+  const UpdateTime = () => {
+    time = new Date().toLocaleTimeString();
+    setTime(time);
+  };
+  setInterval(UpdateTime);
+  return <h1 className="time">{ctime}</h1>;
+}
+
+export default Clock;
