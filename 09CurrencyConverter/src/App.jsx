9b351143d9d3ee60f0329725112cdf340fcd8b75
Prem Choudhary <pk5480524@gmail.com>
2024-09-24 22:40:46 +0530

    last change

--- 09CurrencyConverter/src/App.jsx
+++ 09CurrencyConverter/src/App.jsx
@@ -45,7 +45,7 @@ function App() {
                 label="From"
                 amount={amount}
                 currencyOptions={options}
-                onCurrencyChange={(currency) => setAmount(amount)}
+                onCurrencyChange={() => setAmount(amount)}
                 selectCurrency={from}
                 onAmountChange={(amount) => setAmount(amount)}
               />
